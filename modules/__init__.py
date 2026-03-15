1a70d481b996dbeaa119bbd4dcdee92805be5546
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-03-15 17:14:38 -0600

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,11 @@
+# Modules package initialization
+MODULES = [
+    'vexsqli',
+    'ghauri', 
+    'hakuin',
+    'multiagent',
+    'nosqlmap',
+    'jsql',
+    'sqlninja'
+]
+print(f"[+] {len(MODULES)} módulos disponibles")
